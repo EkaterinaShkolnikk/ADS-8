cd72b4bfa7d183cc52d0c2f81fc40f44bf8d8a02
EkaterinaShkolnikk <75201487+EkaterinaShkolnikk@users.noreply.github.com>
2026-05-31 17:09:59 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,20 @@
 // Copyright 2021 NNTU-CS
 #include <iostream>
+#include <string>
 #include "bst.h"
 
 void makeTree(BST<std::string>& tree, const char* filename);
 void printFreq(BST<std::string>& tree);
 
 int main() {
-    BST<std::string> tree;
-
-    const char* filename = "src/war_peace.txt";
-    makeTree(tree, filename);
-
-    std::cout << "Tree depth: " << tree.depth() << std::endl;
-
-    std::string testWord = "war";
-    int count = tree.search(testWord);
-    std::cout << "Word \"" << testWord << "\" appears " << count << " times." << std::endl;
-
-    printFreq(tree);
-
-    return 0;
+  BST<std::string> tree;
+  const char* filename = "src/war_peace.txt";
+  makeTree(tree, filename);
+  std::cout << "Tree depth: " << tree.depth() << std::endl;
+  std::string testWord = "war";
+  int count = tree.search(testWord);
+  std::cout << "Word \"" << testWord << "\" appears "
+            << count << " times." << std::endl;
+  printFreq(tree);
+  return 0;
 }
